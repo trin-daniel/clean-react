69cabce16e29e798b0807525a1148005deb41ca8
trin-daniel <trindadedanniel63@outlook.com>
2020-09-27 11:16:27 -0300

    feat: add authentication usecase

---
+++ src/domain/usecases/authentication.ts
@@ -0,0 +1,12 @@
+import { AccountModel } from 'domain/models/account-model'
+
+type AuthenticationParams = {
+  email: string,
+  password: string
+}
+
+interface Authentication {
+  auth (params: AuthenticationParams): Promise<AccountModel>
+}
+
+export { Authentication, AuthenticationParams }
