9cd61442ff697d3ddba9975cbb0c78f35593e636
trin-daniel <trindadedanniel63@outlook.com>
2020-09-27 19:22:48 -0300

    refactor: change imports to use module

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
   collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
-  transform: { '.+\\.ts$': 'ts-jest' }
+  transform: { '.+\\.ts$': 'ts-jest' },
+  moduleNameMapper: { '@/(.*)': '<rootDir>/src/$1' }
 }
