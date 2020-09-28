0e50db0aaee00e086c9e027b9307368bf2d45ce7
trin-daniel <trindadedanniel63@outlook.com>
2020-09-28 15:06:43 -0300

    chore: add react configs

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
   coverageDirectory: 'coverage',
-  testEnvironment: 'node',
-  transform: { '.+\\.ts$': 'ts-jest' },
+  testEnvironment: 'jsdom',
+  transform: { '.+\\.(ts|tsx)$': 'ts-jest' },
   moduleNameMapper: { '@/(.*)': '<rootDir>/src/$1' }
 }
